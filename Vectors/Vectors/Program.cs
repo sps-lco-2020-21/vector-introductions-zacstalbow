99c44effb85159139921b5d613082438c0c8ca34
zacstalbow <72789151+zacstalbow@users.noreply.github.com>
2021-04-26 12:47:24 +0100

    Add files via upload

---
+++ Vectors/Vectors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> d1 = new List<double> { 3, 5 };
+            List<double> d2 = new List<double> { 5, -6 };
+            Vector v1 = new Vector(d1);
+            Vector v2 = new Vector(d2);
+            Vector v3 = v1.Add(v2);
+            Console.WriteLine(v3);
+            Console.WriteLine(v1.AlgDotProduct(v2));
+            Console.WriteLine(v1.GeoDotProduct(v2));
+        }
+    }
+}
---
+++ Vectors/Vectors/Vector.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vectors
+{
+    class Vector
+    {
+        List<double> _components;
+        string _type;
+        Dictionary<int, double> _pairs;
+        double _magnitude;
+        const double pi = 3.14159;
+        public Vector(List<double> components)
+        {
+            _components = components;
+        }
+        public List<double> Components { get => _components; set => _components = value; }
+        public string Type 
+        { 
+            get
+            {
+                _type = Components.Count + "-vector";
+                return _type;
+            } 
+        }
+        Dictionary<int, double> Pairs
+        {
+            get
+            {
+                for(int i = 0; i < Components.Count; i++)
+                {
+                    _pairs.Add(i, Components[i]);
+                }
+                return _pairs;
+            }
+            set => _pairs = value;
+        }
+        public double Magnitude
+        {
+            get
+            {
+                double x = 0;
+                List<double> squared = new List<double>();
+                for(int i = 0; i < Components.Count; i++)
+                {
+                    squared.Add(Components[i] * Components[i]);
+                }
+                foreach(double d in squared)
+                {
+                    x += d;
+                }
+                _magnitude = (int)Math.Sqrt(x);
+                return _magnitude;
+            }
+            set => _magnitude = value;
+        }
+        public Vector Add(Vector v)
+        {
+            List<double> d = new List<double>();
+            for(int i = 0; i < Components.Count; i++)
+            {
+                d.Add(Components[i] + v.Components[i]);
+            }
+            Vector result = new Vector(d);
+            return result;
+        }
+        public Vector Multiply(double n)
+        {
+            List<double> d = new List<double>();
+            for(int i = 0; i < Components.Count; i++)
+            {
+                d.Add(Components[i] * n);
+            }
+            Vector result = new Vector(d);
+            return result;
+        }
+        public double AlgDotProduct(Vector v)
+        {
+            List<double> result = new List<double>();
+            double x = 0;
+            for(int i = 0; i < Components.Count; i++)
+            {
+                x += (Components[i] * v.Components[i]);
+            }
+            return x;
+        }
+        public double GeoDotProduct(Vector v)
+        {
+            double theta = ToDegrees(Math.Cos(Magnitude / v.Magnitude));
+            return theta * Magnitude * v.Magnitude;
+        }
+        // i know why this does not work 
+        public double ToDegrees(double radians)
+        {
+            double degrees = (180 * pi) * radians;
+            return degrees;
+        }
+        public override string ToString()
+        {
+            string s = null;
+            for(int i = 0; i < _components.Count; i++)
+            {
+                if(i == 0)
+                {
+                    s += _components[i];
+                }
+                else
+                {
+                    s += "," +_components[i];
+                }
+            }
+            return s;
+        }
+    }
+}
